11075ba226c366e88e326ca1d1bc1e83fbb3cf27
briws <briws@pm.me>
2020-06-19 15:25:58 +0300

    form openes by click

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+let popup1 = document.querySelector("#open1");
+let popup2 = document.querySelector("#open2");
+let popup3 = document.querySelector('#open3');
+let popupWindow = document.querySelector('.popup');
+
+popup1.onclick = function() {
+    popupWindow.classList.remove('hidden');
+  };
+popup2.onclick = function() {
+    popupWindow.classList.remove('hidden');
+  };
+popup3.onclick = function() {
+    popupWindow.classList.remove('hidden');
+  };
