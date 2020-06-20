685d06e8b32d4b1340fbeae469aaaa7c2537c781
briws <briws@pm.me>
2020-06-20 10:38:07 +0300

    close work in js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,7 @@
 let popup1 = document.querySelector("#open1");
 let popup2 = document.querySelector("#open2");
 let popup3 = document.querySelector('#open3');
+let close = document.querySelector('#close');
 let popupWindow = document.querySelector('.popup');
 
 popup1.onclick = function() {
@@ -12,6 +13,9 @@ popup2.onclick = function() {
 popup3.onclick = function() {
     popupWindow.classList.remove('hidden');
   };
+close.onclick = function() {
+    popupWindow.classList.add('hidden');
+}
 addEventListener("keydown", function(event) {
     if (event.keyCode === 27)
         popupWindow.classList.add('hidden');
