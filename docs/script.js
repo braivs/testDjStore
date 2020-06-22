b9d1982ba921bc53c09b63790fab503664160f35
briws <briws@pm.me>
2020-06-22 08:25:45 +0300

    some id tuning

--- docs/script.js
+++ docs/script.js
@@ -2,31 +2,31 @@ let openClick11 = document.querySelector("#openClick11");
 let openClick12 = document.querySelector("#openClick12");
 let openClick13 = document.querySelector('#openClick13');
 let close = document.querySelector('#close');
-let popupWindow = document.querySelector('.popup');
+let popupWindow1 = document.querySelector('#popupWindow1');
 let send = document.querySelector('#send');
 let popupWindow2 = document.querySelector('.popup2');
 let close2 = document.querySelector('#close2');
 let close22 = document.querySelector('#close22');
 
 openClick11.onclick = function() {
-  popupWindow.classList.remove('hidden');
+  popupWindow1.classList.remove('hidden');
   };
 openClick12.onclick = function() {
-  popupWindow.classList.remove('hidden');
+  popupWindow1.classList.remove('hidden');
   };
 openClick13.onclick = function() {
-   popupWindow.classList.remove('hidden');
+   popupWindow1.classList.remove('hidden');
   };
 close.onclick = function() {
-  popupWindow.classList.add('hidden');
+  popupWindow1.classList.add('hidden');
 }
 addEventListener("keydown", function(event) {
   if (event.keyCode === 27)
-    popupWindow.classList.add('hidden');
+    popupWindow1.classList.add('hidden');
     popupWindow2.classList.add('hidden');
   });
 send.onsubmit = function() {
-  popupWindow.classList.add('hidden');
+  popupWindow1.classList.add('hidden');
   popupWindow2.classList.remove('hidden');
 }
 close2.onclick = function() {
